0d13ac497d01ead0b139af7cc0752109ef33107c
Julian Niklaus <jniklaus2@login8.hpc.binf.unibe.ch>
2025-01-30 15:09:29 +0100

    moving files

--- 14-Genespace.R
+++
@@ -1,6 +0,0 @@
-library(GENESPACE)
-args <- commandArgs(trailingOnly = TRUE)
-# get the folder where the genespace files are
-wd <- args[1]
-gpar <- init_genespace(wd = wd, path2mcscanx = "/usr/local/bin", nCores = 20, verbose = TRUE)
-out <- run_genespace(gpar, overwrite = TRUE)
